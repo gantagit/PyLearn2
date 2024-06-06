06a3eff0a97307d806a3d77e8309caf42f0fe2ae
gvk9786 <gvk9786@gmail.com>
2024-06-07 00:06:22 +0100

    Added few more python class files

---
+++ Example_05062024/Lab003.py
@@ -0,0 +1,11 @@
+first_name = "Vijay"
+last_name = "Ganta"
+age = 26
+
+print(first_name, last_name, sep=" & ", end="\t")
+print(age)
+
+print("My first name is", first_name, "and last name", last_name, sep="&", end="\t")
+print(age)
+
+print(input(age))
