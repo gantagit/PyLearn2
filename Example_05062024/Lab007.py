823b83023ebc2bb81fb1bae671f412c5c86446d9
gvk9786 <gvk9786@gmail.com>
2024-06-08 20:43:38 +0100

    Vairables and Data Types

--- Example_05062024/Lab007.py
+++ Example_05062024/Lab007.py
@@ -1,6 +1,19 @@
-# list of complex data types in python
+# variable name should be meaningful
+# variable name should not start with number
+# variable name should not contain any special character
+# variable name should not be a keyword
+# variable name should not be a builtin function
+# variable name should start with small letter
+# variable name should not be a python reserved keyword
+a = 123
+b = a + 15
+print(max(a, b))
+print(min(a, b))
 
-# 1. List
-# 2. Tuple
-# 3. Dictionary
-# 4. Set
+# snake case
+first_name = "Vijay"
+last_name = "Ganta"
+print(first_name, last_name)
+
+
+
---
+++ Example_05062024/Lab008.py
@@ -0,0 +1,32 @@
+# list of complex data types in python
+
+# 1. List
+# 2. Tuple
+# 3. Dictionary
+# 4. Set
+
+# Examples of complex Data types
+
+# List Example
+a = [2, 'abc', 3]
+print(a)
+print(type(a))
+
+# tuple example
+tuple_a = (2, 'abc', 3)
+print(tuple_a)
+print(type(tuple_a))
+
+# append list
+a.append('vijay')
+print(a)
+
+# dictionary example
+dict_a = {'name': 'vijay', 'age': 30}
+print(dict_a)
+print(type(dict_a))
+
+# set example
+set_a = {2, 'abc', 3, 3.14}
+print(set_a)
+print(type(set_a))
