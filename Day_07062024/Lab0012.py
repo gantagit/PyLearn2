3b95df1f19c40b4d17403dff9dd76a78b727949f
gvk9786 <gvk9786@gmail.com>
2024-06-12 14:35:08 +0100

    added files based on the class of 7-june-20204

---
+++ Day_07062024/Lab0012.py
@@ -0,0 +1,16 @@
+# List
+
+shopping_list = ["milk", "eggs", "bread", "apples"]
+print(len(shopping_list))
+print(shopping_list)
+print(type(shopping_list))
+
+shopping_list[0] = "Vijay"
+print(len(shopping_list))
+print(shopping_list)
+print(type(shopping_list))
+
+shopping_list.append("Banana")
+print(len(shopping_list))
+print(shopping_list)
+print(type(shopping_list))
