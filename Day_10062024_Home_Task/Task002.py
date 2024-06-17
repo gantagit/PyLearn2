f05efcd8c0368bd045a1a4d2172f03b56f366c19
gvk9786 <gvk9786@gmail.com>
2024-06-18 00:59:43 +0100

    Task 2 of 10June2024 is done

--- Day_10062024_Home_Task/Task002.py
+++ Day_10062024_Home_Task/Task002.py
@@ -1,6 +1,7 @@
 # Develop a python script that calculates the square and cube of a given number,
 # Example num = 2, square is 4, cube is 8
 
+# solution for first problem of this task
 import math
 
 print("Square of a number is ", math.pow(float(input("Enter a number: ")), 2))
@@ -8,4 +9,9 @@ print("Cube of a number is ",  math.pow(float(input("Enter a number: ")), 3))
 
 # Using Ternary Operator, Create a program that takes two numbers as input and prints 4
 # whether the first number is greater than, less than, or equal to the second number
+
+# solution for second problem of this task
+a = "Enter value for variable a: \t"
+b = "Enter value for variable b: \t"
+print("a is greater than b" if a > b else "a is lesser than b" if a < b else "a is equal to b")
 
