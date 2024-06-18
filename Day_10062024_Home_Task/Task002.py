ab43e6360182b4d582e687898bdca60335dc4dff
gvk9786 <gvk9786@gmail.com>
2024-06-18 01:05:18 +0100

    Task 2 of 10-June-2024 Refactor done

--- Day_10062024_Home_Task/Task002.py
+++ Day_10062024_Home_Task/Task002.py
@@ -12,7 +12,8 @@ print("Cube of a number is ",  math.pow(number, 3))
 # whether the first number is greater than, less than, or equal to the second number
 
 # solution for second problem of this task
-a = int(input("Enter value for variable a: \t"))
-b = int(input("Enter value for variable b: \t"))
+# a = int(input("Enter value for variable a: \t"))
+# b = int(input("Enter value for variable b: \t"))
+a, b = int(input("Enter value for variable a: \t")), int(input("Enter value for variable b: \t"))
 print("a is greater than b" if a > b else "a is lesser than b" if a < b else "a is equal to b")
 
