22ecebbfd46da59d8a33565e087116b093f040cb
gvk9786 <gvk9786@gmail.com>
2024-06-18 01:28:54 +0100

    Task 1 of 10-June-2024

--- Date_10_June_2024_Home_Task/Task001.py
+++ Date_10_June_2024_Home_Task/Task001.py
@@ -1,6 +1,18 @@
 # 1) Explain the difference between = operator and == operator
-
+# here = operator is used for assign a literal to a variable
+a = float(input("Enter value for a : \n"))
+b = float(input("Enter value for b : \n"))
+# here == operator is used for comparison
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # 2) What does the ** operator do in python and how is it used
-
+# Here ** is operator is used as power
+print("a power of b is ", (a**b))
+# the above line is similar to
+# print("a power of b is ", math.pow(a,b))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # 3) What does the ^ operator do in python, and in what context is it commonly used
 
