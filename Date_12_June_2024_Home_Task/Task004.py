49d47d37016ba7203a6d640b1cb317a78cfdec42
gvk9786 <gvk9786@gmail.com>
2024-06-21 11:49:17 +0100

    Home task done of 12th-June-2024

--- Date_12_June_2024_Home_Task/Task004.py
+++ Date_12_June_2024_Home_Task/Task004.py
@@ -4,4 +4,13 @@
 # n = 7
 #
 # 0, 1, 2, 3, 5, 8, 1
+num_1 = 0
+num_2 = 1
+fact = 0
+total_range = int(input("Enter the total range : \t"))
 
+for i in range(total_range):
+    fact = num_1 + num_2
+    num_1 = num_2
+    num_2 = fact
+    print(fact)
