93350d8c7e6839ef827f0db9b1a54ff0fdd202e5
gvk9786 <gvk9786@gmail.com>
2024-06-24 17:49:21 +0100

    added tuple program of 19th-june-2024

--- Date_21_June_2024/Lab064.py
+++ Date_21_June_2024/Lab064.py
@@ -1,13 +1,21 @@
 # Tuple - Collection of items
 
-my_list = [1, 2, 3]
-print(id(my_list))
-print(my_list)
-my_list[0] = 21
-print(id(my_list))
-print(my_list)
+t1 = tuple()  # tuple() create an empty tuple
+print(t1)
 
-my_tuple = (1, 2, 3, 4, 5, 5)
-# my_tuple[0] = 21 # Immutable
-print(my_tuple)
+t2 = tuple(["Vijay", "Ganta", "Pramod"])  # converting a list to tuple
+print(t2)
 
+t5 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)  # tuple packing
+
+t3 = tuple(t1)  # copying a tuple
+print(t3)
+
+t4 = tuple(t2)  # copying a tuple
+print(t4)
+
+t6 = tuple(t5)  # copying a tuple
+print(t6)
+
+del t1  # delete a tuple
+# print(t1) this will not print as the t1 tuple is already deleted
