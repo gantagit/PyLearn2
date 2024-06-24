9b12baad1c63d2fc3fb2bdb533fca57207d93997
gvk9786 <gvk9786@gmail.com>
2024-06-24 17:06:40 +0100

    added tuple program of 19th-june-2024

---
+++ Date_21_June_2024/Lab064.py
@@ -0,0 +1,13 @@
+# Tuple - Collection of items
+
+my_list = [1, 2, 3]
+print(id(my_list))
+print(my_list)
+my_list[0] = 21
+print(id(my_list))
+print(my_list)
+
+my_tuple = (1, 2, 3, 4, 5, 5)
+# my_tuple[0] = 21 # Immutable
+print(my_tuple)
+
