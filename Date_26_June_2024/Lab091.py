6a978e288fe7393892cd0b62e5a5be203b678227
gvk9786 <gvk9786@gmail.com>
2024-06-30 21:02:52 +0100

    optimize programs of class 26-june-2024

--- Date_26_June_2024/Lab091.py
+++ Date_26_June_2024/Lab091.py
@@ -4,23 +4,34 @@ class Calc:
         self.a = a
         self.b = b
 
-    def add(self):
-        return self.a + self.b
-
-    def minus(self):
-        return self.a - self.b
-
-    def multiply(self):
-        return self.a * self.b
-
-    def divide(self):
-        return self.a / self.b
-
-    def modulo(self):
-        return self.a % self.b
+    add = lambda self: self.a + self.b
+    minus = lambda self: self.a - self.b
+    multiply = lambda self: self.a * self.b
+    divide = lambda self: self.a / self.b
+    modulo = lambda self: self.a % self.b
 
     def floor_div(self):
         return self.a // self.b
 
     def power(self):
         return self.a ** self.b
+
+
+# def perform_operation(perform):
+#     match perform:
+#         case 1:
+#             return Calc.add(self)
+#         case 2:
+#             return Calc.minus()
+#         case 3:
+#             print(cal.multiply())
+#         case 4:
+#             print(cal.divide())
+#         case 5:
+#             print(cal.modulo())
+#         case 6:
+#             print(cal.floor_div())
+#         case 7:
+#             print(cal.power())
+#         case _:
+#             print("Invalid Input")
